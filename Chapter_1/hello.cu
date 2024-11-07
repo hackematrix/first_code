b21ad877cec2a04873152ae3f8e7d77cecb37f4d
René Tan <165171058+hackematrix@users.noreply.github.com>
2024-11-07 23:53:53 +0800

    Create hello.cu

---
+++ Chapter_1/hello.cu
@@ -0,0 +1,11 @@
+#include <iostream>
+
+__global__ void kernel() {
+    printf("Hello from CUDA kernel!\n");
+}
+
+int main() {
+    kernel<<<1, 1>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
